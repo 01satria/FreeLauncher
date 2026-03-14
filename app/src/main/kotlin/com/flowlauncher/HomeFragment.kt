6387723ce3ff6ec3390e96ba59c11b571b0d2cc2
Satria <see.cloudys@gmail.com>
2026-03-14 11:40:23 +0700

    new

--- app/src/main/kotlin/com/flowlauncher/HomeFragment.kt
+++ app/src/main/kotlin/com/flowlauncher/HomeFragment.kt
@@ -14,6 +14,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.flowlauncher.databinding.FragmentHomeBinding
+import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
 
 class HomeFragment : Fragment() {
